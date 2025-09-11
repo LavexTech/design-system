3a2e702511389eff51d61ebb12a015a11eed8c81
CaioGiasson <cainitech@Gmail.com>
2025-09-10 22:21:24 -0300

    Correção das constantes

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,25 +1,26 @@
-export default const Constants = {
-   styles: {
-      fontSize: {
-         LARGE: 36,
-         MEDIUM: 24,
-         SMALL: 16,
-         TINY: 12
-      },
-      fontWeight: {
-         BOLD: "700",
-         NORMAL: "400",
-         THIN: "100"
-      },
-      fontFamily: {
-         MAIN: "Roboto",
-      },
-      textColor: {
-         DEFAULT: "#262627",
-         SUCCESS: "#059669",   
-         DANGER: "#DC2626",
-         INFO: "#2563EB"
-      }
-   }
+const Constants = {
+    styles: {
+        fontSize: {
+            LARGE: 36,
+            MEDIUM: 24,
+            SMALL: 16,
+            TINY: 12
+        },
+        fontWeight: {
+            BOLD: "700",
+            NORMAL: "400",
+            THIN: "100"
+        },
+        fontFamily: {
+            MAIN: "Roboto"
+        },
+        textColor: {
+            DEFAULT: "#262627",
+            SUCCESS: "#059669",
+            DANGER: "#DC2626",
+            INFO: "#2563EB"
+        }
+    }
 }
- 
+
+export default Constants
