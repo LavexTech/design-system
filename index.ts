f0ce2f638fce7029fa278abdc26558f67f0b2157
AllanSilva29 <allanjulio61@gmail.com>
2025-09-10 21:54:27 -0300

    Retirada da exportação das props

--- index.ts
+++ index.ts
@@ -1,4 +1,2 @@
 export { HelloWorld } from "./src/components/HelloWorld/HelloWorld";
-export type { HelloWorldProps } from "./src/components/HelloWorld/HelloWorld";
 export { Title1 } from "./src/components/Title1/Title1";
-export type { Title1Props } from "./src/components/Title1/Title1";
