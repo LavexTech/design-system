2c0272c928e20cb62347c496ad164c9c7cb24ca4
AllanSilva29 <allanjulio61@gmail.com>
2025-09-15 16:53:54 -0300

    Estrutura final do componente de Título 3

---
+++ src/components/Title3/Title3.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import Constants from "../../constants/constants";
+
+export interface Title3Props {
+  text: string;
+}
+
+export const Title3: React.FC<Title3Props> = ({ text }) => {
+  return <Text style={styles.title3}>{text}</Text>;
+};
+
+const styles = StyleSheet.create({
+  title3: {
+    fontSize: Constants.styles.fontSize.SMALL,
+    fontWeight: Constants.styles.fontWeight.BOLD,
+    lineHeight: Constants.styles.fontSize.SMALL,
+    fontFamily: Constants.styles.fontFamily.MAIN,
+    color: Constants.styles.textColor.DEFAULT,
+    textAlign: "left",
+    flexWrap: "wrap",
+    flexShrink: 1,
+  },
+});
