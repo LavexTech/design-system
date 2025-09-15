024e9c6064f1c4d226cfb70f76d5e47772f6c0ca
AllanSilva29 <allanjulio61@gmail.com>
2025-09-15 19:39:35 -0300

    Merge '142-criar-componente-info' into main

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export { HelloWorld } from "./src/components/HelloWorld/HelloWorld";
 export { Title1 } from "./src/components/Title1/Title1";
 export { Title2 } from "./src/components/Title2/Title2";
 export { Title3 } from "./src/components/Title3/Title3";
+export { Info } from "./src/components/Info/Info";
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,26 +1,26 @@
 const Constants = {
-    styles: {
-        fontSize: {
-            LARGE: 36,
-            MEDIUM: 24,
-            SMALL: 16,
-            TINY: 12
-        },
-        fontWeight: {
-            BOLD: "700",
-            NORMAL: "400",
-            THIN: "100"
-        },
-        fontFamily: {
-            MAIN: "Roboto"
-        },
-        textColor: {
-            DEFAULT: "#262627",
-            SUCCESS: "#059669",
-            DANGER: "#DC2626",
-            INFO: "#2563EB"
-        }
-    }
-}
+  styles: {
+    fontSize: {
+      LARGE: 36,
+      MEDIUM: 24,
+      SMALL: 18,
+      TINY: 14,
+    },
+    fontWeight: {
+      BOLD: "700",
+      NORMAL: "400",
+      THIN: "100",
+    },
+    fontFamily: {
+      MAIN: "Roboto",
+    },
+    textColor: {
+      DEFAULT: "#262627",
+      SUCCESS: "#059669",
+      DANGER: "#DC2626",
+      INFO: "#2563EB",
+    },
+  },
+};
 
-export default Constants
+export default Constants;
