409f7708dfc1ade57a1932f4cdeafceb91c955fe
AllanSilva29 <allanjulio61@gmail.com>
2025-09-18 20:16:07 -0300

    Exemplos de fonte no index do projeto demo

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -9,8 +9,26 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text style={{ fontFamily: "Roboto-Regular" }}>
-        Edit app/index.tsx to edit this screen.
+      <Text style={{ fontFamily: "Roboto-Regular", fontSize: 20 }}>
+        Roboto Regular
+      </Text>
+      <Text style={{ fontFamily: "Roboto-Medium", fontSize: 20 }}>
+        Roboto Medium
+      </Text>
+      <Text style={{ fontFamily: "Roboto-Bold", fontSize: 20 }}>
+        Roboto Bold
+      </Text>
+      <Text style={{ fontFamily: "Roboto-Light", fontSize: 20 }}>
+        Roboto Light
+      </Text>
+      <Text style={{ fontFamily: "Roboto-Thin", fontSize: 20 }}>
+        Roboto Thin
+      </Text>
+      <Text style={{ fontFamily: "Roboto-ExtraLight", fontSize: 20 }}>
+        Roboto Extra Light
+      </Text>
+      <Text style={{ fontFamily: "Roboto-ExtraLightItalic", fontSize: 20 }}>
+        Roboto Extra Light Italic
       </Text>
     </View>
   );
