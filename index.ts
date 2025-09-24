15493edc74eb11d5081720f0aaab991b55f8b2f5
AllanSilva29 <allanjulio61@gmail.com>
2025-09-24 15:21:58 -0300

    Adição da exportação do input de email

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ export { Title } from "./src/components/Title/Title";
 export { Subtitle } from "./src/components/Subtitle/Subtitle";
 export { TextBox as Text } from "./src/components/Text/Text";
 export { Info } from "./src/components/Info/Info";
+export { InputEmail } from "./src/components/InputEmail/InputEmail";
 export { Card } from "./src/components/Card/Card";
