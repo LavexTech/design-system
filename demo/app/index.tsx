f3a7cad5469d05f61d22b5b57e83ed2ef8df1988
AllanSilva29 <allanjulio61@gmail.com>
2025-09-25 20:41:51 -0300

    Exemplo de uso do botão no projeto demo

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,33 +1,10 @@
-import { View } from "react-native";
-import { Title1 } from "@src/components/Title1/Title1";
-import { Title2 } from "@src/components/Title2/Title2";
-import { Title3 } from "@src/components/Title3/Title3";
-import { Grid } from "@src/components/Grid/Grid";
-import { TextBox as Text } from "@src/components/Text/Text";
+import { Text, View, ScrollView } from "react-native";
+import { ButtonDemo } from "../examples/ButtonDemo/ButtonDemo";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Grid columns={3} gap={4}>
-        <>
-          <Text text="Column 1" />
-          <Title1 text="Hello World" />
-        </>
-        <>
-          <Text text="Column 2" />
-          <Title2 text="Hello World" />
-        </>
-        <>
-          <Text text="Column 3" />
-          <Title3 text="Hello World" />
-        </>
-      </Grid>
-    </View>
+    <ScrollView>
+      <ButtonDemo />
+    </ScrollView>
   );
 }
