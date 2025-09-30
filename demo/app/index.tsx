3f4d87dca1cd8a874137f2a300a2ecf406d4211e
Tigulima <tigulima@gmail.com>
2025-09-30 18:59:34 -0300

    demo/index restaurado

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,33 +1,28 @@
-import { View } from "react-native";
-import { Title1 } from "@src/components/Title1/Title1";
-import { Title2 } from "@src/components/Title2/Title2";
-import { Title3 } from "@src/components/Title3/Title3";
-import { Grid } from "@src/components/Grid/Grid";
+import { ScrollView } from "react-native";
+import { MainTitle } from "@src/components/MainTitle/MainTitle";
+import { Subtitle } from "@src/components/Subtitle/Subtitle";
+import { Title } from "@src/components/Title/Title";
 import { TextBox as Text } from "@src/components/Text/Text";
+import { Info } from "@src/components/Info/Info";
+import { Grid } from "@src/components/Grid/Grid";
+import { Card } from "@src/components/Card/Card";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <ScrollView>
+      <MainTitle text="Main Title" />
+      <Subtitle text="Subtitle" />
+      <Title text="Title" />
+      <Text text="TextBox" />
+      <Info text="Info" />
       <Grid columns={3} gap={4}>
-        <>
-          <Text text="Column 1" />
-          <Title1 text="Hello World" />
-        </>
-        <>
-          <Text text="Column 2" />
-          <Title2 text="Hello World" />
-        </>
-        <>
-          <Text text="Column 3" />
-          <Title3 text="Hello World" />
-        </>
+        <Text text="Column 1" />
+        <Text text="Column 2" />
+        <Text text="Column 3" />
       </Grid>
-    </View>
-  );
+      <Card>
+        <Text text="Card" />
+      </Card>
+    </ScrollView>
+  )
 }
