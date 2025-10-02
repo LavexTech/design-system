95806d99243994d626a213acb195dd67033cb0e7
AllanSilva29 <allanjulio61@gmail.com>
2025-10-02 08:32:53 -0300

    Correção do exemplo de uso

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,17 +1,22 @@
+import React, { useState } from "react";
 import { View } from "react-native";
 import { MainTitle } from "@src/components/MainTitle/MainTitle";
 import { Title } from "@src/components/Title/Title";
 import { Subtitle } from "@src/components/Subtitle/Subtitle";
 import { Grid } from "@src/components/Grid/Grid";
 import { TextBox as Text } from "@src/components/Text/Text";
+import { InputName } from "@src/components/InputName/InputName";
 
 export default function Index() {
+  const [userName, setUserName] = useState("");
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        padding: 20,
       }}
     >
       <Grid columns={3} gap={4}>
@@ -28,6 +33,14 @@ export default function Index() {
           <Subtitle text="Hello World" />
         </>
       </Grid>
+      <View style={{ marginTop: 40, width: '100%', maxWidth: 300 }}>
+        <InputName
+          label="Nome Completo"
+          value={userName}
+          placeholder="Digite seu nome completo"
+          onChange={setUserName}
+        />
+      </View>
     </View>
   );
 }
