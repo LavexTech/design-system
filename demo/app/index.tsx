041e02c6175e9ca10ad89bcd3ceaa21956b0eae2
AllanSilva29 <allanjulio61@gmail.com>
2025-10-02 07:58:24 -0300

    Correção do nome dos componentes

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,7 +1,7 @@
 import { View } from "react-native";
-import { Title1 } from "@src/components/Title1/Title1";
-import { Title2 } from "@src/components/Title2/Title2";
-import { Title3 } from "@src/components/Title3/Title3";
+import { MainTitle } from "@src/components/MainTitle/MainTitle";
+import { Title } from "@src/components/Title/Title";
+import { Subtitle } from "@src/components/Subtitle/Subtitle";
 import { Grid } from "@src/components/Grid/Grid";
 import { TextBox as Text } from "@src/components/Text/Text";
 
@@ -17,15 +17,15 @@ export default function Index() {
       <Grid columns={3} gap={4}>
         <>
           <Text text="Column 1" />
-          <Title1 text="Hello World" />
+          <MainTitle text="Hello World" />
         </>
         <>
           <Text text="Column 2" />
-          <Title2 text="Hello World" />
+          <Title text="Hello World" />
         </>
         <>
           <Text text="Column 3" />
-          <Title3 text="Hello World" />
+          <Subtitle text="Hello World" />
         </>
       </Grid>
     </View>
