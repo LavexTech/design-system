14b8b07727935dcf22dba61d355d94129ce53ec5
AllanSilva29 <allanjulio61@gmail.com>
2025-10-06 16:26:55 -0300

    Exemplo de uso no projeto demo

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { ScrollView } from "react-native";
 import { MainTitle } from "@src/components/MainTitle/MainTitle";
 import { Subtitle } from "@src/components/Subtitle/Subtitle";
@@ -6,6 +7,7 @@ import { TextBox as Text } from "@src/components/Text/Text";
 import { Info } from "@src/components/Info/Info";
 import { Grid } from "@src/components/Grid/Grid";
 import { Card } from "@src/components/Card/Card";
+import { TextList } from "@src/components/TextList/TextList";
 
 export default function Index() {
   return (
@@ -22,6 +24,18 @@ export default function Index() {
       </Grid>
       <Card>
         <Text text="Card" />
+      </Card>
+      <Title text="Text list" />
+      <Card>
+        <TextList
+          texts={[
+            "1x Calça Tiroliro",
+            "2x Blusa xpto etc",
+            "5x Camiseta de banda",
+            "3x Pares de meia",
+            "1x Edredom",
+          ]}
+        />
       </Card>
     </ScrollView>
   )
