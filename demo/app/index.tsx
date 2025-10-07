e8e96cd8db50bf71d08ecfc50b6d1ed719026ff7
AllanSilva29 <allanjulio61@gmail.com>
2025-10-06 21:36:39 -0300

    Exemplo direto no index.tsx do demo

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,4 +1,5 @@
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
+import { useState } from "react";
 import { MainTitle } from "@src/components/MainTitle/MainTitle";
 import { Subtitle } from "@src/components/Subtitle/Subtitle";
 import { Title } from "@src/components/Title/Title";
@@ -6,8 +7,10 @@ import { TextBox as Text } from "@src/components/Text/Text";
 import { Info } from "@src/components/Info/Info";
 import { Grid } from "@src/components/Grid/Grid";
 import { Card } from "@src/components/Card/Card";
+import { InputPassword } from "@src/components/InputPassword/InputPassword";
 
 export default function Index() {
+  const [password, setPassword] = useState('');
   return (
     <ScrollView style={{ marginTop: 20, padding: 20 }}>
       <MainTitle text="Main Title" />
@@ -23,6 +26,15 @@ export default function Index() {
       <Card>
         <Text text="Card" />
       </Card>
+      <View style={{ marginTop: 20 }}>
+        <InputPassword
+          label="Senha"
+          value={password}
+          placeholder="Digite sua senha"
+          onChange={setPassword}
+        />
+      </View>
+      
     </ScrollView>
   )
 }
