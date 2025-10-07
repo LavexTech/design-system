d6d44bd64307a6a8fcebf6bd8484770304af4264
AllanSilva29 <allanjulio61@gmail.com>
2025-10-07 16:25:58 -0300

    Global.css movido para o src

--- demo/app/_layout.tsx
+++ demo/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
+import '@src/global.css';
 
 export default function RootLayout() {
   return <Stack />;
