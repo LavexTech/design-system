ea89c703ad4f121906ea95f7b566ba405506a37e
AllanSilva29 <allanjulio61@gmail.com>
2025-10-13 21:54:08 -0300

    Remoção do GluestacksUiProvider para coloca-lo diretamente nos componentes importados do design-system

--- demo/app/_layout.tsx
+++ demo/app/_layout.tsx
@@ -1,12 +1,8 @@
 import { Stack } from "expo-router";
-import { GluestackUIProvider } from '@src/ui/gluestack-ui-provider';
 import '@src/global.css';
 
 export default function RootLayout() {
   return (
-    // TODO: Mover para nível componentes, a fim de isentar os projetos de borda de importarem essa dependência diretamente
-    <GluestackUIProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </GluestackUIProvider>
+    <Stack screenOptions={{ headerShown: false }} />
   );
 }
