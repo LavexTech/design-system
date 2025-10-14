d435e37d64b11c0b7ae0cb7c406fb89c6beda347
CaioGiasson <cainitech@Gmail.com>
2025-10-13 21:07:17 -0300

    Update demo/app/_layout.tsx

--- demo/app/_layout.tsx
+++ demo/app/_layout.tsx
@@ -4,6 +4,7 @@ import '@src/global.css';
 
 export default function RootLayout() {
   return (
+    // TODO: Mover para nível componentes, a fim de isentar os projetos de borda de importarem essa dependência diretamente
     <GluestackUIProvider>
       <Stack screenOptions={{ headerShown: false }} />
     </GluestackUIProvider>
