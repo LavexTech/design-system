df9c86f59b3cb3f9fbb4c6a23b41eb8f490bf1ce
AllanSilva29 <allanjulio61@gmail.com>
2025-10-15 17:46:12 -0300

    Merge '20-criar-componente-input' into 18-criar-componente-inputphone

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,33 +1,102 @@
-import { ScrollView, View, StyleSheet } from "react-native";
-import { useState } from 'react';
+import { View, ScrollView } from "react-native";
+import { useState } from "react";
+
 import { MainTitle } from "@src/components/MainTitle/MainTitle";
 import { Subtitle } from "@src/components/Subtitle/Subtitle";
 import { Title } from "@src/components/Title/Title";
 import { TextBox as Text } from "@src/components/Text/Text";
 import { Info } from "@src/components/Info/Info";
-import { Grid } from "@src/components/Grid/Grid";
+import { Grid, GridItem } from "@src/components/Grid/Grid";
 import { Card } from "@src/components/Card/Card";
+import { Input } from "@src/components/Input/Input";
 import { InputPhone } from "@src/components/InputPhone/InputPhone";
 
 export default function Index() {
   const [phone, setPhone] = useState('');
-  
+  const [value, setValue] = useState("");
+
   return (
-    <ScrollView style={{ marginTop: 20, padding: 20 }}>
-      <MainTitle text="Main Title" />
-      <Subtitle text="Subtitle" />
-      <Title text="Title" />
-      <Text text="TextBox" />
-      <Info text="Info" />
-      <Grid columns={3} gap={4}>
-        <Text text="Column 1" />
-        <Text text="Column 2" />
-        <Text text="Column 3" />
+    <ScrollView
+      style={{ flex: 1, padding: 20 }}
+      contentContainerStyle={{ paddingBottom: 40 }}
+      showsVerticalScrollIndicator={true}
+    >
+      <Grid columns={1} gap={2}>
+        <MainTitle text="Main Title" />
+        <Subtitle text="Subtitle" />
+        <Title text="Title" />
+        <Text text="TextBox" />
+        <Info text="Info" />
+        <Title text="Grid Components" />
+        <Grid columns={3} gap={4}>
+          <Text text="Column 1" />
+          <Text text="Column 2" />
+          <Text text="Column 3" />
+        </Grid>
+        <Subtitle text="Grid with Cards (2 columns)" />
+        <Grid columns={2} gap={4}>
+          <Card>
+            <Text text="Card 1" />
+          </Card>
+          <Card>
+            <Text text="Card 2" />
+          </Card>
+          <Card>
+            <Text text="Card 3" />
+          </Card>
+          <Card>
+            <Text text="Card 4" />
+          </Card>
+        </Grid>
+
+        <Subtitle text="Custom Column Spans (10 columns grid)" />
+        <Grid columns={10} gap={4}>
+          <GridItem colSpan={3}>
+            <Card>
+              <></>
+            </Card>
+          </GridItem>
+          <GridItem colSpan={5}>
+            <Card>
+              <></>
+            </Card>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Card>
+              <></>
+            </Card>
+          </GridItem>
+          <GridItem colSpan={4}>
+            <Card>
+              <></>
+            </Card>
+          </GridItem>
+          <GridItem colSpan={6}>
+            <Card>
+              <></>
+            </Card>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Card>
+              <></>
+            </Card>
+          </GridItem>
+          <GridItem colSpan={4}>
+            <Card>
+              <></>
+            </Card>
+          </GridItem>
+          <GridItem colSpan={4}>
+            <Card>
+              <></>
+            </Card>
+          </GridItem>
+        </Grid>
       </Grid>
       <Card>
-        <Text text="Card" />
+        <Text text="Card" level="warning" />
       </Card>
-      <View style={styles.section}>
+      <View>
         <Subtitle text="Com Mensagem de Erro Customizada" />
         <Info text="Exemplo com mensagem de erro personalizada" />
         <InputPhone
@@ -36,24 +105,19 @@ export default function Index() {
             errorMessage="Por favor, insira um telefone válido"
         />
       </View>
+      <View style={{ marginTop: 20 }}>
+        <Input
+          label="Input"
+          value={value}
+          placeholder="Enter Text here..."
+          onChange={setValue}
+          validation={validateInput}
+          errorMessage="Input must be less than 7 characters" />
+      </View>
     </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({
-
-    section: {
-        marginBottom: 30,
-        backgroundColor: "white",
-        padding: 16,
-        borderRadius: 8,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.1,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-});
+const validateInput = (value: string) => {
+  return value.length < 7;
+}
