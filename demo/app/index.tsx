a3162ee5fc2fbff8c0b4023d66343a0799490b4e
AllanSilva29 <allanjulio61@gmail.com>
2025-10-15 21:02:46 -0300

    Atualização do exemplo de uso

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, ScrollView, Button } from "react-native";
+import { View, ScrollView } from "react-native";
 import { MainTitle } from "@src/components/MainTitle/MainTitle";
 import { Subtitle } from "@src/components/Subtitle/Subtitle";
 import { Title } from "@src/components/Title/Title";
@@ -7,6 +7,8 @@ import { TextBox as Text } from "@src/components/Text/Text";
 import { Modal } from "@src/components/Modal/Modal";
 import { Info } from "@src/components/Info/Info";
 import { Grid } from "@src/components/Grid/Grid";
+import { Card } from "@src/components/Card/Card";
+import { Button } from "@src/components/Button/Button";
 
 export default function Index() {
   const [showModal, setShowModal] = useState(false);
@@ -19,15 +21,26 @@ export default function Index() {
         <Title text="Title" />
         <Text text="TextBox" />
         <Info text="Info" />
+        <View style={{ marginTop: 20, marginBottom: 20 }}>
+          <Button text="Mostrar Modal" onClick={() => setShowModal(true)} />
+        </View>
         <Grid columns={3} gap={4}>
           <Text text="Column 1" />
           <Text text="Column 2" />
           <Text text="Column 3" />
         </Grid>
+        <Card>
+          <Text text="Card" />
+        </Card>
+        <Grid columns={3} gap={2}>
+          <Button variant="default" text="Button" onClick={() => {}} />
+          <Button variant="danger" text="Button" onClick={() => {}} />
+          <Button variant="success" text="Button" onClick={() => {}} />
+          <Button variant="default-outline" text="Button" onClick={() => {}} />
+          <Button variant="danger-outline" text="Button" onClick={() => {}} />
+          <Button variant="success-outline" text="Button" onClick={() => {}} />
+        </Grid>
 
-        <View style={{ marginTop: 20 }}>
-          <Button title="Mostrar Modal" onPress={() => setShowModal(true)} />
-        </View>
       </ScrollView>
 
       {showModal && (
