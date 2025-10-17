040a9f00951efb114a7d4dd6b2dabd3348070821
AllanSilva29 <allanjulio61@gmail.com>
2025-10-17 20:36:48 -0300

    Melhorias no Accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,57 +1,66 @@
-import React from "react";
+import React from "react"
 import {
   Accordion as GluestackAccordion,
-  AccordionItem,
+  AccordionItem as GluestackAccordionItem,
   AccordionHeader,
   AccordionTrigger,
   AccordionTitleText,
   AccordionContent,
   AccordionIcon,
 } from "../../ui/accordion"
-import { Divider } from "../../ui/divider";
-import { AddIcon, RemoveIcon } from '../../ui/icon';
-import Constants from "../../constants/constants";
+import { Divider } from "../../ui/divider"
+import { AddIcon, RemoveIcon } from '../../ui/icon'
+import Constants from "../../constants/constants"
 
-export interface AccordionProps {
-  text: string;
-  children: React.ReactNode;
-  onClick?: () => void;
+type AccordionItemProps = {
+  id: string,
+  title: string,
+  children: React.ReactNode
+};
+
+type AccordionProps = {
+  children: React.ReactNode
+};
+
+export const AccordionItem: React.FC<AccordionItemProps> = ({ id, title, children }) => {
+  return (
+    <GluestackAccordionItem value={id}>
+      <AccordionHeader>
+        <AccordionTrigger>
+          {({ isExpanded }: { isExpanded: boolean }) => {
+            return (
+              <>
+                <AccordionTitleText>
+                  {title}
+                </AccordionTitleText>
+                {isExpanded ? (
+                  <AccordionIcon as={RemoveIcon} />
+                ) : (
+                  <AccordionIcon as={AddIcon} />
+                )}
+              </>
+            );
+          }}
+        </AccordionTrigger>
+      </AccordionHeader>
+      <AccordionContent>
+        {children}
+      </AccordionContent>
+    </GluestackAccordionItem>
+  )
 }
 
-export const Accordion: React.FC<AccordionProps> = ({ text, children, onClick }) => {
+export const Accordion: React.FC<AccordionProps> = ({ children }) => {
   return (
     <>
-    <GluestackAccordion 
-      variant="unfilled" 
-      isCollapsible={true}
-      style={{ backgroundColor: Constants.styles.backgroundColor.WHITE }}
-    >
-      <AccordionItem value="item-1">
-        <AccordionHeader>
-          <AccordionTrigger>
-            {({ isExpanded }: { isExpanded: boolean }) => {
-              return (
-                <>
-                  <AccordionTitleText>
-                    {text}
-                  </AccordionTitleText>
-                  {isExpanded ? (
-                    // TODO: Trocar GluestackIcon pelo nosso componente Icon
-                    <AccordionIcon as={RemoveIcon} />
-                  ) : (
-                    <AccordionIcon as={AddIcon} />
-                  )}
-                </>
-              );
-            }}
-          </AccordionTrigger>
-        </AccordionHeader>
-        <AccordionContent>
-          {children}
-        </AccordionContent>
-      </AccordionItem>
-    </GluestackAccordion>
-    <Divider />
+      <GluestackAccordion 
+        variant="unfilled" 
+        isCollapsible={true}
+        style={{ backgroundColor: Constants.styles.backgroundColor.WHITE }}
+      >
+        {children}
+      </GluestackAccordion>
+      <Divider />
     </>
   )
 }
