315361d9b42b6a99d2ebb5aa22fb437ba8c5b32f
AllanSilva29 <allanjulio61@gmail.com>
2025-10-17 20:37:04 -0300

    Exemplo do novo uso do Accordion

--- demo/app/index.tsx
+++ demo/app/index.tsx
@@ -4,9 +4,7 @@ import { Subtitle } from "@src/components/Subtitle/Subtitle";
 import { Title } from "@src/components/Title/Title";
 import { TextBox as Text } from "@src/components/Text/Text";
 import { Info } from "@src/components/Info/Info";
-import { Grid } from "@src/components/Grid/Grid";
-import { Card } from "@src/components/Card/Card";
-import { Accordion } from "@src/components/Accordion/Accordion";
+import { Accordion, AccordionItem } from "@src/components/Accordion/Accordion";
 
 export default function Index() {
   return (
@@ -16,25 +14,16 @@ export default function Index() {
       <Title text="Title" />
       <Text text="TextBox" />
       <Info text="Info" />
-      <Accordion text="Accordion">
-        <Text text="Accordion Content Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos." />
-      </Accordion>
-      <Grid columns={3} gap={4}>
-        <Text text="Column 1" size="small" />
-        <Text text="Column 2"  />
-        <Text text="Column 3" size="large"/>
-      </Grid>
-      <Card>
-        <Text text="Card" />
-      </Card>
-      <Accordion text="Accordion">
-        <Text text="Accordion Content Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos." />
-      </Accordion>
-      <Accordion text="Accordion">
-        <Text text="Accordion Content Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos." />
-      </Accordion>
-      <Accordion text="Accordion">
-        <Text text="Accordion Content Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos." />
+      <Accordion>
+        <AccordionItem id="item-1" title="Primeiro Item">
+          <Text size="small" text="Conteúdo do primeiro item do accordion. Este item pode ser expandido e colapsado independentemente." />
+        </AccordionItem>
+        <AccordionItem id="item-2" title="Segundo Item">
+          <Text size="small" text="Conteúdo do segundo item. Cada item tem seu próprio estado de expansão/colapso." />
+        </AccordionItem>
+        <AccordionItem id="item-3" title="Terceiro Item">
+          <Text size="small" text="Conteúdo do terceiro item. Todos os itens estão dentro de um único componente Accordion." />
+        </AccordionItem>
       </Accordion>
     </ScrollView>
   )
