fac42d3a097c59777b6da5e1bdda66d65fe0fa7f
Allan Silva <allanjulio61@gmail.com>
2025-10-23 17:39:35 -0400

    Update index.ts
    Co-authored-by: Tigulima <62662386+tigulima@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ export { TextBox as Text } from "./src/components/Text/Text";
 export { Info } from "./src/components/Info/Info";
 export { Card } from "./src/components/Card/Card";
 export { Alert } from "./src/components/Alert/Alert";
-export { Modal } from "./src/components/Modal/Modal";
 export { List } from "./src/components/List/List";
 export { Button } from "./src/components/Button/Button";
 export { Grid } from "./src/components/Grid/Grid";
