bbf9c8ef1c86bb5db66ade62b24f3504692c6d29
Tigulima <tigulima@gmail.com>
2025-10-28 19:55:18 -0300

    pequenos ajustes do componente acordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -8,7 +8,7 @@ import {
   AccordionContent,
   AccordionIcon,
 } from "../../ui/accordion"
-import { Divider } from "../../ui/divider"
+import { Divider } from "../Divider/Divider"
 import { AddIcon, RemoveIcon } from '../../ui/icon'
 import Constants from "../../constants/constants"
 
@@ -24,44 +24,44 @@ type AccordionProps = {
 
 export const AccordionItem: React.FC<AccordionItemProps> = ({ id, title, children }) => {
   return (
-    <GluestackAccordionItem value={id}>
-      <AccordionHeader>
-        <AccordionTrigger>
-          {({ isExpanded }: { isExpanded: boolean }) => {
-            return (
-              <>
-                <AccordionTitleText>
-                  {title}
-                </AccordionTitleText>
-                {isExpanded ? (
-                  <AccordionIcon as={RemoveIcon} />
-                ) : (
-                  <AccordionIcon as={AddIcon} />
-                )}
-              </>
-            );
-          }}
-        </AccordionTrigger>
-      </AccordionHeader>
-      <AccordionContent>
-        {children}
-      </AccordionContent>
-    </GluestackAccordionItem>
+    <>
+      <GluestackAccordionItem value={id}>
+        <AccordionHeader>
+          <AccordionTrigger>
+            {({ isExpanded }: { isExpanded: boolean }) => {
+              return (
+                <>
+                  <AccordionTitleText>
+                    {title}
+                  </AccordionTitleText>
+                  {isExpanded ? (
+                    <AccordionIcon as={RemoveIcon} />
+                  ) : (
+                    <AccordionIcon as={AddIcon} />
+                  )}
+                </>
+              );
+            }}
+          </AccordionTrigger>
+        </AccordionHeader>
+        <AccordionContent>
+          {children}
+        </AccordionContent>
+      </GluestackAccordionItem>
+      <Divider />
+    </>
   )
 }
 
 export const Accordion: React.FC<AccordionProps> = ({ children }) => {
   return (
-    <>
-      <GluestackAccordion 
-        variant="unfilled" 
-        isCollapsible={true}
-        style={{ backgroundColor: Constants.styles.backgroundColor.WHITE }}
-      >
-        {children}
-      </GluestackAccordion>
-      <Divider />
-    </>
-  )
+    <GluestackAccordion 
+      variant="unfilled" 
+      isCollapsible={true}
+      style={{ backgroundColor: Constants.styles.backgroundColor.WHITE }}
+    >
+      {children}
+    </GluestackAccordion>
+)
 }
 
