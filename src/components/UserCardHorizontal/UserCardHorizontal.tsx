ae237beda1ffcab400b7b291c39bf8e23da1585d
AllanSilva29 <allanjulio61@gmail.com>
2025-10-30 21:50:19 -0300

    UserCardHorizontal com ajustes atualizados

--- src/components/UserCardHorizontal/UserCardHorizontal.tsx
+++ src/components/UserCardHorizontal/UserCardHorizontal.tsx
@@ -1,80 +1,65 @@
-import React from "react";
-import { View, Image, Text as RNText, StyleSheet } from "react-native";
-import { Card } from "../Card/Card";
-import { TextBox } from "../Text/Text";
-import Constants from "../../constants/constants";
+import React from "react"
+import { View, StyleSheet } from "react-native"
+import { Card } from "../Card/Card"
+import { TextBox } from "../Text/Text"
+import Constants from "../../constants/constants"
+import { Stars } from "../Stars/Stars"
+import { Info } from "../Info/Info"
+import { Image } from "../Image/Image"
+import { Grid, GridItem } from "../Grid/Grid"
 
-export interface User {
-  id: string;
-  name: string;
-  profileImage: string;
-  ordersCount: number;
-  rating: number;
+type User = {
+  id: string,
+  name: string,
+  profileImage: string,
+  ordersCount: number,
+  rating: number
 }
 
-export interface UserCardHorizontalProps {
-  user: User;
-  onClick?: () => void;
+type UserCardHorizontalProps = {
+  user: User,
+  onClick?: () => void
 }
 
 export const UserCardHorizontal: React.FC<UserCardHorizontalProps> = ({
   user,
   onClick,
 }: UserCardHorizontalProps) => {
-  const renderStars = (rating: number) => {
-    const stars = [];
-    const fullStars = Math.round(rating);
-
-    for (let i = 0; i < 5; i++) {
-      if (i < fullStars) {
-        stars.push(
-          <RNText key={i} style={styles.star}>
-            ★
-          </RNText>
-        );
-      } else {
-        stars.push(
-          <RNText key={i} style={styles.starEmpty}>
-            ★
-          </RNText>
-        );
-      }
-    }
-
-    return stars;
-  };
-
   return (
     <Card onClick={onClick}>
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image
-            source={{ uri: user.profileImage }}
-            style={styles.profileImage}
-            resizeMode="cover"
-          />
-        </View>
-
-        <View style={styles.infoContainer}>
+      <Grid columns={3}>
+        <GridItem colSpan={1}>
+          <View>
+            <Image
+              src={user.profileImage}
+              type="circle"
+              size="md"
+            />
+          </View>
+        </GridItem>
+        <GridItem colSpan={2}>
           <View style={styles.nameContainer}>
             <TextBox text={user.name} />
           </View>
-
           <View style={styles.ordersContainer}>
-            <TextBox text={`${user.ordersCount} pedidos feitos`} />
+            <Info text={`${user.ordersCount} pedidos feitos`} />
           </View>
-
           <View style={styles.ratingContainer}>
-            <View style={styles.starsContainer}>{renderStars(user.rating)}</View>
-            <RNText style={styles.ratingText}>
-              {Math.round(user.rating)}/5
-            </RNText>
+            <View style={styles.starsContainer}>
+              <Stars rating={user.rating} size={16} />
+            </View>
+            <View>
+              <Info
+                text={user.rating.toFixed(1)}
+                size="small"
+              />
+            </View>
           </View>
-        </View>
-      </View>
+        </GridItem>
+      </Grid>
     </Card>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -91,14 +76,6 @@ const styles = StyleSheet.create({
     borderWidth: Constants.styles.borderWidth.REGULAR,
     borderColor: Constants.styles.borderColor.LIGHT,
   },
-  profileImage: {
-    width: "100%",
-    height: "100%",
-  },
-  infoContainer: {
-    flex: 1,
-    justifyContent: "center",
-  },
   nameContainer: {
     marginBottom: Constants.styles.spacing.TINY,
   },
@@ -110,19 +87,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   starsContainer: {
-    flexDirection: "row",
-    alignItems: "center",
     marginRight: Constants.styles.spacing.TINY,
-  },
-  star: {
-    fontSize: 14,
-    color: "#262627",
-    marginHorizontal: 1,
-  },
-  starEmpty: {
-    fontSize: 14,
-    color: "#DEE2E6",
-    marginHorizontal: 1,
   },
   ratingText: {
     fontSize: Constants.styles.fontSize.SMALL,
